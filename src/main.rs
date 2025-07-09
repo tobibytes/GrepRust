512d908762355d6397e48f030c730b135db7f368
Oluwatobi <oldwhisper98@icloud.com>
2025-07-09 00:10:20 -0700

    removed unused libraries to make it lightweight

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
-use anyhow::{Context, Result};
 use clap::Parser;
-
-
 #[derive(Parser)]
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 fn main() -> Result<()>{
-
     let args = Cli::parse(); 
     let content = std::fs::read_to_string(&args.path)
     .with_context(|| format!("Error reading: `{}`", args.path.display()))?;
-
     for line in content.lines() {
         if line.contains(&args.pattern) {       
             println!("{}", line);
         }
-
     }
     Ok(())
 }
